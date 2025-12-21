d788e7960e6b94c2f9ceb72194b82da922101edb
Andre <aj.automacao@outlook.com>
2025-12-20 22:09:41 -0300

    feat: add @vercel/speed-insights integration

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import ScrollToTop from "./components/ScrollToTop";
 import Index from "./pages/Index";
 import Sobre from "./pages/Sobre";
@@ -30,6 +31,7 @@ const App = () => (
           <Route path="/contato" element={<Contato />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
+        <SpeedInsights />
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
